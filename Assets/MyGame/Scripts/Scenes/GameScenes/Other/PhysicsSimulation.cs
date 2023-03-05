023316d035b78dafce3d2d93c3cbcb95d83beed3
phantasia <phantasia03@gmail.com>
2023-03-05 16:31:39 +0800

    unity 2022 updata

--- Assets/MyGame/Scripts/Scenes/GameScenes/Other/PhysicsSimulation.cs
+++ Assets/MyGame/Scripts/Scenes/GameScenes/Other/PhysicsSimulation.cs
@@ -7,11 +7,11 @@ public class PhysicsSimulation : MonoBehaviour
     [ContextMenu("Run Simulation ")]
     public void RunSimulation()
     {
-        Physics.autoSimulation = false;
+       // Physics.autoSimulation = false;
 
         for (int i = 0; i < 1000; i++)
             Physics.Simulate(Time.deltaTime);
 
-        Physics.autoSimulation = true;
+       // Physics.autoSimulation = true;
     }
 }
